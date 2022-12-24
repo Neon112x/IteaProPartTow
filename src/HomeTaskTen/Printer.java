dface831ebee16db6e05521456bc4ddb7cbea461
Neon112x <112348743+Neon112x@users.noreply.github.com>
2022-12-24 15:47:46 +0200

    Initial commit
    (cherry picked from commit 0ed8843875157578167883c9689de82b44ccb99d)

--- src/HomeTaskTen/Printer.java
+++ src/HomeTaskTen/Printer.java
@@ -2,7 +2,10 @@ package HomeTaskTen;
 
 public class Printer extends Collar{
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 0ed8843 (Initial commit)
     public void textBlue (String value) {
 
         System.out.println(getANSI_BLUE() + value + getANSI_RESET());
